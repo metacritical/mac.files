58d8a97d40a3b33c0e7a248ef72af43a097228b3
Pankaj Doharey <pankajdoharey@live.com>
2017-07-03 20:33:35 +0530

    Updates gitstats with a prompt case

--- .oh_my_bash/bin/gitstats.go
+++ .oh_my_bash/bin/gitstats.go
@@ -62,7 +62,7 @@ func main(){
 		case "s", "status":
 			fmt.Print(processStatus(gitStatus()))
 		case "p", "prompt":
-			fmt.Print(gitBranch(), processStatus(gitBranch()))
+			fmt.Print(gitBranch(), processStatus(gitStatus()))
 		default:
 			fmt.Print(execCommand(args[1]))
 		}
