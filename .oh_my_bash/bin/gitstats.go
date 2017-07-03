f729436c600a7482c47844d282845218407fd674
Pankaj Doharey <pankajdoharey@live.com>
2017-07-03 20:00:31 +0530

    Updats gitstats utility for creating prompt

--- .oh_my_bash/bin/gitstats.go
+++ .oh_my_bash/bin/gitstats.go
@@ -5,45 +5,50 @@ import (
 	"fmt"
 	"regexp"
 	"os/exec"
+	"strings"
 )
 
 
-func execCommand(command string) []byte{
+func execCommand(command string) string{
 	gitExec := exec.Command("git", command)
 	outPut, _  := gitExec.Output()
-	return outPut
+	return string(outPut)
 }
 
 func getSymbol(name string) string{
 	return	map[string]string{
-		"untracked":"\xe2\x9b\x94\xef\xb8\x8f\x0a" , "alliswell":"☯️ ", "recycle":"♻️ ", "hazard":"☢" , "deleted":"\xe2\x9d\x8c\x0a",
-		"smiley":"🙂 ", "flag":"🚩 " , "repoahead":"\xe2\x8f\xab\x0a" , "prohibited":"\xf0\x9f\x9a\xab\x0a" , "changes":"\xE2\x9A\xA0\xEF\xB8\x8F",
+		"untracked":"\xe2\x9b\x94\xef\xb8\x8f\x0a", "alliswell":"☯️ ", "recycle":"♻️ ",
+		"hazard":"☢", "deleted":"\xe2\x9d\x8c\x0a", "smiley":"🙂 ", "flag":"🚩 " ,
+		"repoahead":"\xe2\x8f\xab\x0a", "prohibited":"\xf0\x9f\x9a\xab\x0a",
+		"changes":"\xE2\x9A\xA0\xEF\xB8\x8F", "no-vc":"\xe2\x9d\x97\xef\xb8\x8f\x0a",
 	}[name]
 }
 
-func processStatus(status []byte) string{
-	noVC, _ := regexp.Match("Not a git repository", status)
-	unTracked, _ := regexp.Match("Untracked files", status)
-	notStaged, _ := regexp.Match("Changes not staged for commit", status)
+func processStatus(status string) string{
+	prompt := make([]string, 1)
+	var noVC = regexp.MustCompile("Not a git repository")
+	var unTracked = regexp.MustCompile("Untracked files")
+	var notStaged = regexp.MustCompile("Changes not staged for commit")
 
 	switch {
-		case noVC :
-		return "NO-VC"
-		case unTracked :
-		return getSymbol("untracked")
-		case notStaged :
-		return getSymbol("changes")
+		case noVC.MatchString(status) :
+		prompt = append(prompt, getSymbol("no-vc"))
+		case unTracked.MatchString(status):
+		prompt = append(prompt, getSymbol("untracked"))
+		case notStaged.MatchString(status) :
+		prompt = append(prompt, getSymbol("changes"))
 		default:
-		return "Fatal:"
+		prompt = append(prompt, getSymbol("flag"))
 	}
 
+	return strings.Join(prompt, " ")
 }
 
-func gitBranch() []byte{
+func gitBranch() string{
 	return execCommand("branch")
 }
 
-func gitStatus() []byte{
+func gitStatus() string{
 	return execCommand("status")
 }
 
@@ -53,7 +58,7 @@ func main(){
 	if len(args) > 1 {
 		switch args[1]{
 		case "b", "branch":
-			fmt.Print(string(gitBranch()))
+			fmt.Print(gitBranch())
 		case "s", "status":
 			fmt.Print(processStatus(gitStatus()))
 		default:
