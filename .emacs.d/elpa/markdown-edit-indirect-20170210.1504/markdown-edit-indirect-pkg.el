87efef2c1455e44979ce801913bd31fe47836aab
Pankaj Doharey <pankajdoharey@gmail.com>
2018-07-03 05:58:48 +0530

    Deleted downloaded .emacs local libs.

--- .emacs.d/elpa/markdown-edit-indirect-20170210.1504/markdown-edit-indirect-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-edit-indirect" "20170210.1504" "Edit markdown code block in a separate buffer" '((emacs "24.3") (markdown-mode "2.1") (edit-indirect "0.1.4")) :url "https://github.com/emacs-pe/markdown-edit-indirect.el" :keywords '("convenience"))
--- .emacs.d/elpa/pretty-lambdada-20170307.940/pretty-lambdada-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pretty-lambdada" "20170307.940" "Show the word `lambda' as the Greek letter." 'nil :url "https://www.emacswiki.org/emacs/download/pretty-lambdada.el" :keywords '("convenience" "display"))
