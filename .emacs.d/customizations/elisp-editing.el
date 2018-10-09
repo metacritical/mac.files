6af97f506acb22d9fb3e08680882c5c01d66e008
Pankaj Doharey <pankajdoharey@gmail.com>
2018-10-09 19:54:57 +0530

    Parinfer mode, GRAALVM PATH.

--- .emacs.d/customizations/elisp-editing.el
+++ .emacs.d/customizations/elisp-editing.el
@@ -8,11 +8,11 @@
 ;; (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 ;; (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
-(add-hook 'clojure-mode-hook #'parinfer-mode)
-(add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-(add-hook 'common-lisp-mode-hook #'parinfer-mode)
-(add-hook 'scheme-mode-hook #'parinfer-mode)
-(add-hook 'lisp-mode-hook #'parinfer-mode)
+;; (add-hook 'clojure-mode-hook #'parinfer-mode)
+;; (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+;; (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+;; (add-hook 'scheme-mode-hook #'parinfer-mode)
+;; (add-hook 'lisp-mode-hook #'parinfer-mode)
 
 ;;Smart parens
 ;; (require 'smartparens-config)
